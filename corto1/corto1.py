6cfd0536cbbf263329064a45aa78b80f31664363
201602863-Luis-Fernando-Montenegro <2760150450101@ingenieria.usac.edu.gt>
2020-06-09 12:40:36 -0600

    Entrega corto 1

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,2 +1,43 @@
 
 print("Hola")
+Collatz = 'collatz.txt'
+N = 863
+n = 2
+#ln = []
+archivo = open(Collatz, 'w')
+def numeropar(num):
+    #global p
+    
+    c = num%2
+    if c == 0:
+        #print("Es par")
+        n = num//2
+        #num += 1
+        ln.append(n)
+        #numeropar(num)
+        buscaruno(n)
+
+    else:
+        #print("No es par")
+        n = num*3+1
+        #num += 1
+        ln.append(n)
+        buscaruno(n)
+        #numeropar(num)
+        
+def buscaruno(n):
+    if n!= 1:
+        numeropar(n)
+
+
+while(N>=n):
+    print(n)
+    ln=[n]
+    numeropar(n)
+    n += 1
+    archivo.write(str(ln))
+    archivo.write("\n")
+    print(ln)
+    del ln
+archivo.close()
+
